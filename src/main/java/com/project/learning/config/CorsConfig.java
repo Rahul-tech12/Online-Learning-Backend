b9247331b851f4484c69bdfc28473613fcb54d90
Rahul Choudhary <1122rahulchoudhary@gmail.com>
2026-02-01 20:38:16 +0530

    Update allowed origins in CORS configuration

--- src/main/java/com/project/learning/config/CorsConfig.java
+++ src/main/java/com/project/learning/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration config=new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("https://online-learning-frontend-teal.vercel.app"));
         config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(false);
